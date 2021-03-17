a5040e08d69489fc7dad69b78f2667b8fb713491
Rico Putra P <rico.putra@outlook.co.id>
2021-03-17 14:41:35 +0700

    Implement middleware

--- index.js
+++ index.js
@@ -2,18 +2,33 @@ const { response, request } = require('express');
 const express = require('express');
 const app = express();
 
-// use body-parsing middleware to populate req.body
+// use body-parsing MIDDLEWARE to populate req.body
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
+/**
+ * Middleware for checking user login status
+ */
+const checkLoginStatus = (request, response, next) => {
+  console.log("EXECUTED FIRST!!! - LOGGED IN!");
+  request.token = "ABCKASJSASASAMSA";
+  next();
+}
+app.use(checkLoginStatus);
 
 const port = 3000;
 const userRouter = require('./router/users');
 
 // routing: `app.METHOD(PATH, HANDLER)`| METHOD: get, post, put, delete
 app.get('/', (request, response) => {
+  console.log("EXECUTED AFTER MIDDLEWARE!!! - GET HELLO!")
   // send: memberikan response dalam bentuk string dan tipe data lainnya untuk ditampilkan ke dalam halaman web browser
-  response.send('Hello World!');
+  const kelas = {
+    id: 1,
+    name: "Express JS",
+    token: request.token
+  }
+  response.json(kelas);
 });
 
 app.get('/about', (request, response) => {
