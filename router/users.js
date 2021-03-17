470d14ba2c17927f1b541a6dcf0a8c5164de544b
Rico Putra P <rico.putra@outlook.co.id>
2021-03-17 14:12:01 +0700

    Refactor json response for users

--- router/users.js
+++ router/users.js
@@ -11,11 +11,31 @@ let users = [
 
 router.route('/users')
   .get((request, response) => {
-    response.json(users);
+    // response.json(users);
+    if (users.length > 0) {
+      response.json({
+        status: true,
+        data: users,
+        method: request.method,
+        url: request.url
+      })
+    }
+    else {
+      response.json({
+        status: false,
+        message: "Data users masih kosong"
+      })
+    }
   })
   .post((request, response) => {
     users.push(request.body);
-    response.send(users);
+    response.json({
+      status: true,
+      message: "Data user telah berhasil disimpan!",
+      data: users,
+      method: request.method,
+      url: request.url
+    });
   })
 
 router.put('/users/:id', (request, response) => {
@@ -26,13 +46,25 @@ router.put('/users/:id', (request, response) => {
       return user;
     }
   });
-  response.json(users);
+  response.json({
+    status: true,
+    message: "Data user telah berhasil diperbarui!",
+    data: users,
+    method: request.method,
+    url: request.url
+  });
 });
 
 router.delete('/users/:id', (request, response) => {
   const id = request.params.id;
   users = users.filter(user => user.id != id);
-  response.json(users)
+  response.json({
+    status: true,
+    message: "Data user telah berhasil dihapus!",
+    data: users,
+    method: request.method,
+    url: request.url
+  });
 })
 
 module.exports = router;
